0d3d8f813025aca1160ba5ecfb0b9492fc0c505b
RavijyotsnaPachauri <90235394+RavijyotsnaPachauri@users.noreply.github.com>
2025-12-26 20:27:27 +0000

    variables and datatypes

--- basics/variables.js
+++ basics/variables.js
@@ -2,6 +2,18 @@ const accId=144553 // value cannot be changed
 let accEmail="ravi@gmail.com"
 var accPassword="12345"
 accCity="Jaipur"
+let accState // javascript treats this as undefined
 
 // accId=2
 console.log(accId)
+
+accEmail="abc@gmail.com"
+accPassword="2121"
+accCity="Chennai"
+
+/*
+Note: Prefer not to use 'var' because of issues in block scope and functional scope
+*/
+
+console.table([accEmail,accPassword,accCity])
+console.log(state)
