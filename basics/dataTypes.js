6572b99ba139fd2000b52df51939ce9b9afc9669
RavijyotsnaPachauri <90235394+RavijyotsnaPachauri@users.noreply.github.com>
2025-12-27 20:44:05 +0000

    Datatypes and ECMA

---
+++ basics/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all JS code as newer version of JS
+
+// alert(3+3) // we are using nodejs, not browser(it works in browser)
+
+let name="Ravijyotsna"
+let age=25
+let isLoggedIn=false
+let state=null // null means empty
+
+//   DATATYPES
+
+// number ==> 2 to the power 53
+// bigint
+// string ==> ""
+// boolean ==>true/false
+// null ==> standalone value
+// undefined ==> not assigned value yet
+// symbol ==> unique
+
+// object
+
+console.log(typeof "abc")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
